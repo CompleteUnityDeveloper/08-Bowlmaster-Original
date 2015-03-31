2f53d7fff86d4817fbd04b470a915f0164fbb45c
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-03-31 14:14:34 +0100

    3D Sphere As Bowling Ball

---
+++ Assets/Ball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public float launchSpeed;
+	
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+	
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.velocity = new Vector3 (0, 0, launchSpeed);
+		
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
