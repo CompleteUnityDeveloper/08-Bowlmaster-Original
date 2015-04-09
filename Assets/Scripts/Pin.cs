32ac517d87b402578e408f8cae4e06b3fabdf88c
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-04-09 17:22:12 +0100

    Working Around Nested Prefabs

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+
+	public float standingThreshold = 3f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (name + IsStanding ());
+	}
+	
+	public bool IsStanding () {
+		Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+		float tiltInX = Mathf.Abs(rotationInEuler.x);
+		float tiltInZ = Mathf.Abs(rotationInEuler.z);
+
+		if (tiltInX < standingThreshold && tiltInZ < standingThreshold) {
+			return true;
+		} else {
+			return false;
+		}
+
+		return true;
+	}
+}
