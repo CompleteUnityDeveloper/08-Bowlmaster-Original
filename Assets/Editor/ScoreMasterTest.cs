700c4ff13162616f57860e1fe9a86b09535fd403
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-04-24 19:43:18 +0100

    BM27 Install Unity Test Tools

---
+++ Assets/Editor/ScoreMasterTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ScoreTest {
+
+	[Test]
+	public void PassingTest () {
+		Assert.AreEqual (1, 1);
+	}
+}
