8348fadf98c1e8c4fe04d2a5a28a6e3330c16645
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-04-28 22:40:14 +0100

    Red > Green > Refactor

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -6,7 +6,14 @@ using UnityEngine;
 [TestFixture]
 public class ActionMasterTest {
 
+	private ActionMaster actionMaster;
 	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+
+	[SetUp]
+	public void Setup () {
+		actionMaster = new ActionMaster ();
+	}
 
 	[Test]
 	public void T00PassingTest () {
@@ -15,7 +22,17 @@ public class ActionMasterTest {
 
 	[Test]
 	public void T01OneStrikeReturnsEndTurn () {
-		ActionMaster actionMaster = new ActionMaster ();
 		Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+	}
+
+	[Test]
+	public void T02Bowl8ReturnsTidy () {
+		Assert.AreEqual (tidy, actionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T04Bowl28SpareReturnsEndTurn () {
+		actionMaster.Bowl (8);
+		Assert.AreEqual (endTurn, actionMaster.Bowl(2));
 	}
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -2,19 +2,28 @@
 using System.Collections;
 
 public class ActionMaster {
-
 	public enum Action {Tidy, Reset, EndTurn, EndGame};
+
+	// private int[] bowls = new int[21];
+	private int bowl = 1;
 
 	public Action Bowl (int pins) {
 		if (pins < 0 || pins > 10) {throw new UnityException ("Invalid pins");}
 
-		// Other behaviour here
+		// Other behaviour here, e.g. last frame.
 
 		if (pins == 10) {
+			bowl += 2;
 			return Action.EndTurn;
 		}
 
-		// Other behaviour here
+		if (bowl % 2 != 0) { // Mid frame (or last frame)
+			bowl += 1;
+			return Action.Tidy;
+		} else if (bowl % 2 == 0) { // End of frame
+			bowl += 1;
+			return Action.EndTurn;
+		}
 
 		throw new UnityException ("Not sure what action to return!");
 	}
