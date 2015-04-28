41e89b9248a2daa9d72f2db90ddc7c846e3428f3
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-04-28 22:11:29 +0100

    BM27 Making Your First Test Pass

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest {
+
+	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+	[Test]
+	public void T00PassingTest () {
+		Assert.AreEqual (1, 1);
+	}
+
+	[Test]
+	public void T01OneStrikeReturnsEndTurn () {
+		ActionMaster actionMaster = new ActionMaster ();
+		Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+	}
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMaster {
+
+	public enum Action {Tidy, Reset, EndTurn, EndGame};
+
+	public Action Bowl (int pins) {
+		if (pins < 0 || pins > 10) {throw new UnityException ("Invalid pins");}
+
+		// Other behaviour here
+
+		if (pins == 10) {
+			return Action.EndTurn;
+		}
+
+		// Other behaviour here
+
+		throw new UnityException ("Not sure what action to return!");
+	}
+}
