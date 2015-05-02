da204a940bf5148b5149c264163e8679ce64eac4
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-05-02 09:42:28 +0100

    Finishing Our Control Code

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -9,6 +9,8 @@ public class ActionMasterTest {
 	private ActionMaster actionMaster;
 	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
 	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+	private ActionMaster.Action reset = ActionMaster.Action.Reset;
+	private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
 
 	[SetUp]
 	public void Setup () {
@@ -34,5 +36,42 @@ public class ActionMasterTest {
 	public void T04Bowl28SpareReturnsEndTurn () {
 		actionMaster.Bowl (8);
 		Assert.AreEqual (endTurn, actionMaster.Bowl(2));
+	}
+
+	[Test]
+	public void T05CheckResetAtStrikeInLastFrame () {
+		int[] rolls = {1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1};
+		foreach (int roll in rolls) {
+			actionMaster.Bowl (roll);
+		}
+		Assert.AreEqual (reset, actionMaster.Bowl (10));
+	}
+
+	[Test]
+	public void T06CheckResetAtSpareInLastFrame () {
+		int[] rolls = {1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1};
+		foreach (int roll in rolls) {
+			actionMaster.Bowl (roll);
+		}
+		actionMaster.Bowl (1);
+		Assert.AreEqual (reset, actionMaster.Bowl (9));
+	}
+
+	[Test]
+	public void T07YouTubeRollsEndInEndGame () {
+		int[] rolls = {8,2, 7,3, 3,4, 10, 2,8, 10, 10, 8,0, 10, 8,2};
+		foreach (int roll in rolls) {
+			actionMaster.Bowl (roll);
+		}
+		Assert.AreEqual (endGame, actionMaster.Bowl(9));
+	}
+
+	[Test]
+	public void T08GameEndsAtBowl20 () {
+		int[] rolls = {1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1};
+		foreach (int roll in rolls) {
+			actionMaster.Bowl (roll);
+		}
+		Assert.AreEqual (endGame, actionMaster.Bowl (1));
 	}
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -4,13 +4,25 @@ using System.Collections;
 public class ActionMaster {
 	public enum Action {Tidy, Reset, EndTurn, EndGame};
 
-	// private int[] bowls = new int[21];
+	private int[] bowls = new int[21];
 	private int bowl = 1;
 
 	public Action Bowl (int pins) {
 		if (pins < 0 || pins > 10) {throw new UnityException ("Invalid pins");}
 
-		// Other behaviour here, e.g. last frame.
+		bowls [bowl - 1] = pins;
+
+		if (bowl == 21) {
+			return Action.EndGame;
+		}
+
+		// Hanld last-frame special cases
+		if (bowl >= 19 && Bowl21Awarded ()) {
+			bowl += 1;
+			return Action.Reset;
+		} else if (bowl == 20 && ! Bowl21Awarded()) {
+			return Action.EndGame;
+		}
 
 		if (pins == 10) {
 			bowl += 2;
@@ -26,5 +38,10 @@ public class ActionMaster {
 		}
 
 		throw new UnityException ("Not sure what action to return!");
+	}
+	 
+	private bool Bowl21Awarded () {
+		// Remember that arrays start counting at 0
+		return (bowls [19-1] + bowls [20-1] >= 10);
 	}
 }
