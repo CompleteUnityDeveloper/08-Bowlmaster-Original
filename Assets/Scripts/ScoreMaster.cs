9fbb075a9ac699560570d30778c7985feebd409c
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-05-19 07:53:13 +0100

    BM36 Creating A Testable Architecture

---
+++ Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreMaster {
+
+	public static List<int> ScoreFrames (List<int> rolls) {
+		List<int> frameList = new List<int> ();
+
+		// Your code here
+
+		return frameList;
+	}
+}
