900ce499166c1f38f27529a393228f37fd9a6495
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-06-02 10:41:58 +0100

    BM43 An Array Of UI Text.mp4

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	public Text[] rollTexts, frameTexts;
+
+	// Use this for initialization
+	void Start () {
+		rollTexts [0].text = "X";
+		frameTexts [0].text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
