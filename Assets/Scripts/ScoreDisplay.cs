46d8682f52943961a4d48962fa6610ab7c49199d
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-06-05 20:15:02 +0100

    BM48 Designing Your Own Tests.mp4

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -22,7 +22,23 @@ public class ScoreDisplay : MonoBehaviour {
 
 	public static string FormatRolls (List<int> rolls) {
 		string output = "";
-		// Your code here
+			
+		for (int i = 0; i < rolls.Count; i++) {
+			int box = output.Length + 1;							// Score box 1 to 21 
+
+			if (rolls[i] == 0) {									// Always enter 0 as -
+				output += "-";
+			} else if (box % 2 == 0 && rolls[i-1]+rolls[i] == 10) {	// SPARE anywhere
+				output += "/";	
+			} else if (box >= 19 && rolls[i] == 10)	{				// STRIKE in frame 10
+				output += "X";
+			} else if (rolls[i] == 10) {							// STRIKE in frame 1-9
+				output += "X ";
+			} else {
+				output += rolls[i].ToString();						// Normal 1-9 bowl
+			}
+		}
+	
 		return output;
 	}
 }
