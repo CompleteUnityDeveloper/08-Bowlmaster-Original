4748fec91d724920b200690c17ab30d51022ea4b
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-06-05 21:11:11 +0100

    BM49 Final Fixes & Finishing Off.mp4

--- Assets/Scripts/BallDragLaunch.cs
+++ Assets/Scripts/BallDragLaunch.cs
@@ -15,7 +15,10 @@ public class BallDragLaunch : MonoBehaviour {
 
 	public void MoveStart (float amount) {
 		if ( ! ball.inPlay) {
-			ball.transform.Translate (new Vector3 (amount, 0, 0));
+			float xPos = Mathf.Clamp(ball.transform.position.x + amount, -50f, 50f);
+			float yPos = ball.transform.position.y;
+			float zPos = ball.transform.position.z;
+			ball.transform.position = new Vector3 (xPos, yPos, zPos);
 		}
 	}
 
